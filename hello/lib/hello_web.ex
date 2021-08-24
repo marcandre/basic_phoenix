f005f730b5dfba9d65b7771bd320209a2141c64a
Marc-Andre Lafortune <github@marc-andre.ca>
2021-08-23 23:49:38 -0400

    Add missing defs

--- hello/lib/hello_web.ex
+++ hello/lib/hello_web.ex
@@ -42,6 +42,22 @@ defmodule HelloWeb do
     end
   end
 
+  def live_view do
+    quote do
+      use Phoenix.LiveView
+      unquote(view_helpers())
+      import HelloWeb.LiveHelpers
+    end
+  end
+
+  def live_component do
+    quote do
+      use Phoenix.LiveComponent
+      unquote(view_helpers())
+      import HelloWeb.LiveHelpers
+    end
+  end
+
   def router do
     quote do
       use Phoenix.Router
