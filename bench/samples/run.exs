f7b4bbaa5511766f5069e6789e57f0fca61ac3b0
Marc-Andre Lafortune <github@marc-andre.ca>
2021-11-30 16:08:56 -0500

    map_join

--- bench/samples/run.exs
+++ bench/samples/run.exs
@@ -1,10 +1,10 @@
 list = Enum.to_list(1..10_000)
-map_fun = fn i -> [i, i * i] end
+map_fun = &to_string/1
 
 Benchee.run(
   %{
-    "flat_map" => fn -> Enum.flat_map(list, map_fun) end,
-    "map.flatten" => fn -> list |> Enum.map(map_fun) |> List.flatten() end
+    "map_join" => fn -> Enum.map_join(list, "/", map_fun) end,
+    "map.join" => fn -> list |> Enum.map(map_fun) |> Enum.join("/") end
   },
   time: 10,
   memory_time: 2
